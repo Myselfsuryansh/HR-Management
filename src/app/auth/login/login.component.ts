3fd157872f6d3408a66b8583693e466771ddf61b
myselfsuryansh <suryanshsinhabth11@gmail.com>
2024-01-22 13:32:21 +0530

    New changes in dropdown

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/service/auth.service';
